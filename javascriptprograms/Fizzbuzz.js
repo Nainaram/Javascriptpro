f46b522ff482a6d2abda48561bdcdbf5b0f306a2
NainaRam <nainaram7980@gmail.com>
2023-12-22 12:27:45 +0530

    added new file

---
+++ javascriptprograms/Fizzbuzz.js
@@ -0,0 +1,20 @@
+var output = [];
+var count  = 1;
+ function fizzbuzz(){
+      while( count <= 100){
+          if(count%3==0 && count%5 ==0){
+              output.push("FIZZBUZZ");
+          }
+          else if(count%3 ==0){
+              output.push("FIZZ");
+          }
+          else if(count%5 == 0){
+              output.push("buzz");
+          }
+          else{
+              output.push(count);
+          }
+          count++;
+          
+      }
+ }
